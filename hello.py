e7dd93846571660fa2e79a1e0a84af5e989354f3
wallenh <38147201+wallenh@users.noreply.github.com>
2018-11-14 13:12:10 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,4 @@ print("Hello World")
 stockprice = 10 * 5
 print(stockprice)
 print("I am adding some information to this python script that will make its way to my github account!")
+print("Now, one more time, I am adding information to confirm that the whole online merge things works.")
