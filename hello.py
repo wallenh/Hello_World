290dd53c8fc0596f766b7409beec11021072d4af
wallenh <38147201+wallenh@users.noreply.github.com>
2018-11-14 20:17:44 -0500

    real first version

--- hello.py
+++ hello.py
@@ -1,7 +1,21 @@
+#importing necessary packages
 from math import *
+from tkinter import *
 
+#definition of functions
 def commodityprice(price,variance,time,Variable):
     pricez = price * exp(-.5 * variance * time + sqrt(variance) * sqrt(time) * Variable)
-    return print(pricez)
+    return pricez
 
-print(commodityprice(11,.01,1,2))
+#creation of widget
+root = Tk()
+
+#creation of text for widget
+a = str(round(commodityprice(5,.02,1,-2),3))
+b = "There is a good chance that prices will remain above " + a + ".\n Regardless, there is always a possibility that prices could drop below " + a + "."
+#widget
+w = Label(root,text = b,width=80,height=40)
+w.pack()
+
+#tkinter event loop
+root.mainloop()
---
+++ tkinter_hello.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+a = 10*.98
+w = Label(root, text=a)
+w.pack()
+
+root.mainloop()
